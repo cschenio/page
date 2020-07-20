328e4507e220b131a85817c1359bb67aec35f2f1
Chia-Sheng Chen <mail@cschen.io>
2020-07-21 01:48:03 +0800

    Init

---
+++ Program.fs
@@ -0,0 +1,7 @@
+﻿open System
+open Suave
+
+[<EntryPoint>]
+let main argv =
+    startWebServer defaultConfig (Successful.OK "Hello there, this is Chia-Sheng!")
+    0
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Page
+
+This is the page for cschenio.
+
+## Roadmap
+
+### P0
+- Migrate my old site here
+
+### P1
+- Add some fancy toys
