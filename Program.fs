3f268166a8073ff41c862b167b1ca2d514178442
Chia-Sheng Chen <mail@cschen.io>
2020-07-21 02:50:42 +0800

    Add /pow and /add to path

--- Program.fs
+++ Program.fs
@@ -1,7 +1,22 @@
-﻿open System
-open Suave
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+
+let addTwoString n = sprintf "2 + %d = %d" n (n + 2)
+
+let powOfTwoString n = sprintf "2 ^ %d = %d" n (pown 2 n)
 
 [<EntryPoint>]
 let main argv =
-    startWebServer defaultConfig (Successful.OK "Hello there, this is Chia-Sheng!")
+
+    let n = 6
+
+    let app =
+        choose
+            [ GET >=> choose
+                          [ path "/pow" >=> OK(powOfTwoString n)
+                            path "/add" >=> OK(addTwoString n) ] ]
+
+    startWebServer defaultConfig app
     0
